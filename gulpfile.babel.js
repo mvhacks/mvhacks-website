0cd7ad2952fc92237e4fd65cf67228e903265209
plusCubed <plusCubed@users.noreply.github.com>
2016-05-28 23:19:38 -0700

    Initial commit - migrate to Web Starter Kit

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -213,7 +213,7 @@ gulp.task('default', ['clean'], cb =>
 // Run PageSpeed Insights
 gulp.task('pagespeed', cb =>
   // Update the below URL to the public URL of your site
-  pagespeed('example.com', {
+  pagespeed('mv-hacks.com', {
     strategy: 'mobile'
     // By default we use the PageSpeed Insights free (no API key) tier.
     // Use a Google Developer API key if you have one: http://goo.gl/RkN0vE

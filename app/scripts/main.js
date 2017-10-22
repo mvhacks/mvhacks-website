318c7927f78ade32e567d7eb3a8dd09f68b7757a
plusCubed <pluscubed@users.noreply.github.com>
2017-10-21 22:54:33 -0700

    Final event commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -78,8 +78,47 @@
       });
   }
 
+  function countdown(dateEnd) {
+    var timer, days, hours, minutes, seconds;
+
+    dateEnd = new Date(dateEnd);
+    dateEnd = dateEnd.getTime();
+
+    if ( isNaN(dateEnd) ) {
+      return;
+    }
+
+    timer = setInterval(calculate, 1000);
+
+    function calculate() {
+      var dateStart = new Date();
+      var timeRemaining = parseInt((dateEnd - dateStart.getTime()) / 1000)
+
+      if ( timeRemaining >= 0 ) {
+        days    = parseInt(timeRemaining / 86400);
+        timeRemaining   = (timeRemaining % 86400);
+        hours   = parseInt(timeRemaining / 3600);
+        timeRemaining   = (timeRemaining % 3600);
+        minutes = parseInt(timeRemaining / 60);
+        timeRemaining   = (timeRemaining % 60);
+        seconds = parseInt(timeRemaining);
+
+        //document.getElementById("days").innerHTML    = parseInt(days, 10);
+        document.getElementById("hours").innerHTML   = ("0" + hours).slice(-2);
+        document.getElementById("minutes").innerHTML = ("0" + minutes).slice(-2);
+        document.getElementById("seconds").innerHTML = ("0" + seconds).slice(-2);
+      } else {
+        return;
+      }
+    }
+
+    function display(days, hours, minutes, seconds) {}
+  }
+
+  countdown('2017-10-21T20:00:00-07:00');
+
   // Down button event listener
-  $('#down-button').click(function() {
+  /*$('#down-button').click(function() {
     $('.mdl-layout__content')
       .stop()
       .animate(
@@ -88,7 +127,7 @@
         'swing',
         function() {}
       );
-  });
+  });*/
 
   $('.staggered-grid').masonry({
     // options
